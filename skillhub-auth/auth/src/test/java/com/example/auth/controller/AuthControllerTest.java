91aa2ba98417dc90fbc233ecf277d5ab5a929c1b
Moustansir-H <hassanalymoustansir@gmail.com>
2026-04-24 01:52:19 +0400

    sonar

--- skillhub-auth/auth/src/test/java/com/example/auth/controller/AuthControllerTest.java
+++ skillhub-auth/auth/src/test/java/com/example/auth/controller/AuthControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -33,6 +34,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class AuthControllerTest {
 
     private static final String REGISTER_URL = "/api/auth/register";
